742d5016ae5232fcd437cd2b5c7b0d9ceef810f3
waishendev <waishendev@gmail.com>
2026-01-25 09:03:32 +0800

    x

--- backend/ecommerce_gentlegurl_backend_api/database/seeders/PermissionSeeder.php
+++ backend/ecommerce_gentlegurl_backend_api/database/seeders/PermissionSeeder.php
@@ -63,7 +63,7 @@ class PermissionSeeder extends Seeder
             'ecommerce.products' => ['view', 'create', 'update', 'delete'],
             'ecommerce.shop-menu' => ['view', 'create', 'update', 'delete'],
             'ecommerce.services-menu' => ['view', 'create', 'update', 'delete'],
-            'ecommerce.services-pages' => ['view', 'update'],
+            'ecommerce.services-pages' => ['view', 'create', 'update', 'delete'],
             'ecommerce.marquees' => ['view', 'create', 'update', 'delete'],
             'ecommerce.sliders' => ['view', 'create', 'update', 'delete'],
             'ecommerce.stores' => ['view', 'create', 'update', 'delete'],
