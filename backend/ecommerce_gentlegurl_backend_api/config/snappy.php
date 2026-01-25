76bc5486835529e1eafb1776fa3cc9db27be70be
waishendev <waishendev@gmail.com>
2026-01-25 23:54:24 +0800

    x

--- backend/ecommerce_gentlegurl_backend_api/config/snappy.php
+++ backend/ecommerce_gentlegurl_backend_api/config/snappy.php
@@ -3,7 +3,7 @@
 return [
     'pdf' => [
         'enabled' => true,
-        'binary' => env('WKHTMLTOPDF_BINARY', '/usr/bin/wkhtmltopdf'),
+        'binary' => env('WKHTMLTOPDF_BINARY', '/usr/local/bin/wkhtmltopdf'),
         'timeout' => false,
         'options' => [
             'encoding' => 'UTF-8',
@@ -15,7 +15,7 @@ return [
     ],
     'image' => [
         'enabled' => true,
-        'binary' => env('WKHTMLTOIMAGE_BINARY', '/usr/bin/wkhtmltoimage'),
+        'binary' => env('WKHTMLTOIMAGE_BINARY', '/usr/local/bin/wkhtmltoimage'),
         'timeout' => false,
         'options' => [],
         'env' => [],
